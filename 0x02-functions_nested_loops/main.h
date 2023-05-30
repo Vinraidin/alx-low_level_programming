2e0d074d095e9108ebad82961b6eb09b149aadb3
brandon <brandonthoolsi04@gmail.com>
2023-05-30 03:32:08 -0700

    update

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef main_h
+#define main_h
+
+int _puychar(char);
+void print_alphabet(void);
+void print_aalphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_beauer(void);
+void time_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
