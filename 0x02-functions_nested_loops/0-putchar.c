814ea740532dfac1e4a867841561f9a73eca5759
root <brandonthoolsi04@gmail.com>
2023-07-18 06:10:53 -0700

    Functions nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,16 +4,17 @@
  * is written by mbah
  * Return: 0
  */
+
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+_puychar('_');
+_puychar('p');
+_puychar('u');
+_puychar('t');
+_puychar('c');
+_puychar('h');
+_puychar('a');
+_puychar('r');
+_puychar('\n');
 return (0);
 }
